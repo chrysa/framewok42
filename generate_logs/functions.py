b12f55cd529e86227b787fa947bc883441225298
chrysa <greau.anthony@gmail.com>
2015-07-11 12:19:36 +0200

    internation a corriger

---
+++ generate_logs/functions.py
@@ -0,0 +1,13 @@
+#-*-coding:utf-8 -*-
+
+def info_load_log_message(request):
+    return 'chargement de la page ' + str(request.META['PATH_INFO']) + ' par ' + str(request.user)
+
+def info_login_class_log_message(request):
+    return 'connection classique par ' + str(request.user)
+
+def info_login_ldap_log_message(request):
+    return 'connection ldap par ' + str(request.user)
+
+def info_logout_log_message(request):
+    return 'deconnection par ' + str(request.user)
