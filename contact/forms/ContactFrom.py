3db0ccce5b997bb7f2de728ab42dae5de6bf3714
chrysa <greau.anthony@gmail.com>
2015-07-21 21:32:03 +0200

    merge avec fix

--- contact/forms/ContactFrom.py
+++ contact/forms/ContactFrom.py
@@ -1,9 +1,28 @@
 #-*-coding:utf-8 -*-
+"""
+:module: contact.forms.ContactForm
+:synopsis: Define ContactForm
+
+:moduleauthor: anthony greau <anthony@gmail.com>
+:created: 01/07/2015
+:update: 21/07/2015
+"""
 from django import forms
 from django.utils.translation import ugettext as _
 
 
 class ContactForm(forms.Form):
+    """
+    this class define field of contact form
+    :param forms.Form: contain all function for define a field
+    :type forms.Form: form object
+    :return: None
+    :rtype: None
+
+    :var email: define email field
+    :var subject: define subject field
+    :var message: define message field
+    """
     email = forms.EmailField(
         label=_("mail_adress"),
         widget=forms.TextInput(
