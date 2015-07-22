326d47d5da92bd62f97625a776712b3a57157a4a
chrysa <greau.anthony@gmail.com>
2015-07-22 11:17:25 +0200

    on commente petit a petit

--- contact/urls.py
+++ contact/urls.py
@@ -1,4 +1,12 @@
 #-*-coding:utf-8 -*-
+"""
+:module: contact.urls
+:synopsis: root URL for contact
+
+:moduleauthor: anthony greau <greau.anthony@gmail.com>
+:created: 01/07/2015
+:update: 21/07/2015
+"""
 from django.conf.urls import patterns
 from django.conf.urls import url
 
