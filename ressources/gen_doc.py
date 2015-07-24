f1b0756c228a4fd2815cfb5339847eb3cd42b6a3
chrysa <greau.anthony@gmail.com>
2015-07-25 01:26:27 +0200

    doc config

---
+++ ressources/gen_doc.py
@@ -0,0 +1,19 @@
+import os
+from subprocess import call
+project = os.listdir('.')
+
+if not os.path.isdir('doc'):
+    call('mkdir doc', shell=True)
+    call('sphinx-quickstart', shell=True)
+    file = "doc/source/conf.py"
+    result = open(file, 'r').read().replace("html_theme = 'alabaster'", "html_theme = 'classic'").replace("#html_title = None", "html_title = None").replace("#html_show_copyright = True", "html_show_copyright = True").replace("#html_last_updated_fmt = '%b %d, %Y'", "html_last_updated_fmt = '%b %d, %Y'").replace("#html_show_sourcelink = True", "html_show_sourcelink = True")
+    open(file, 'w').write(result)
+    open(file, 'a').write("\nimport django\n\nsys.path.insert(0, os.path.abspath('../..'))\nos.environ.setdefault('DJANGO_SETTINGS_MODULE', 'framework.settings')\ndjango.setup()")
+
+call('sphinx-apidoc -f -o doc/source/ .', shell=True)
+for app in project:
+    if os.path.isfile('{}/__init__.py'.format(app)) and app[0] != ('.' or '_'):
+        call('sphinx-apidoc -f -o doc/source/ {}/'.format(app), shell=True)
+
+call('cd doc && make html', shell=True)
+call('cd doc && make latex', shell=True)
