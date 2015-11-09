b2ae8547acca2e90afbab6d82d8bccb31f369cc1
chrysa <chrysa@users.noreply.github.com>
2015-11-09 12:11:08 +0100

    Update settings.py

--- framework/settings.py
+++ framework/settings.py
@@ -8,9 +8,9 @@ DEBUG = True
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'greau.anthony@gmail.com'
-DEFAULT_EMAIL_FROM = 'greau.anthony@gmail.com'
-EMAIL_HOST_PASSWORD = 'uxavvwvlhvkkdapo'
+EMAIL_HOST_USER = ''
+DEFAULT_EMAIL_FROM = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
